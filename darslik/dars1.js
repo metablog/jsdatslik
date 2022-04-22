22e19c9367196ef792b2f7781cdd7516c49a73bd
Khusan <khu10735@naver.com>
2022-04-22 16:01:30 +0900

    grade

--- darslik/dars1.js
+++ darslik/dars1.js
@@ -111,25 +111,95 @@
 
 
 
-function grade(baho){
-    if(baho >= 90){
-        console.log("A")
-    }else if(baho >=80){
-        console.log("B")
-    }else if(baho >=70){
-        console.log("C")
-    }else if(baho >=60){
-        console.log("D")
-    }else if(baho >=40){
-        console.log("E")
-    }else{
-        console.log("F")
-    }
+// function grade(baho){
+//     if(baho >= 90){
+//         console.log("A")
+//     }else if(baho >=80){
+//         console.log("B")
+//     }else if(baho >=70){
+//         console.log("C")
+//     }else if(baho >=60){
+//         console.log("D")
+//     }else if(baho >=40){
+//         console.log("E")
+//     }else{
+//         console.log("F")
+//     }
     
+// }
+// grade(90)
+// grade(80)
+// grade(70)
+// grade(60)
+// grade(40)
+// grade(39)
+
+// 1. str1 va str2 stringlar berilgan agar str1 va str2 uzunligi bir biriga teng bolsa str1 ni aks holda str2 ni qaytaruvchi funksiya tuzing
+// function s(name, name1){
+//     if(name.length === name1.length){
+//         console.log(name)
+//         console.log(name.length)
+//     }else{
+//         console.log(name1)
+//         console.log(name1.length)
+//     }    
+// }
+// s("alli","vali")
+// s("eshmat","toshmat")
+
+
+//2. string berilgan uning oxirgi va boshidagi qiymatlarni olib tashlang
+// function str(a){
+//     console.log(a.slice(1,4))
+// }
+// str("hello")
+
+// function str1(b){
+//     console.log(b.slice(1,2))
+// }
+// str1("abs")
+// function str2(c){
+//     console.log(c.slice(1,1))
+// }
+// str2("va")
+
+//5. Sonni toq yoki juftligini tekshiring
+// function s(num){
+//     if(num % 2 === 0){
+//         console.log("juft son")
+//     }else{
+//         console.log("tog son")
+//     }
+// }
+// s(2)
+// s(3)
+
+//4. 1-7 raqamlarga mos hafta kunini chiqazing
+
+let haftaKunlari = function(hafta){
+    if(hafta == 1){
+        console.log("dushanba")
+    }else if(hafta == 2){
+        console.log("seshanba")
+    }else if(hafta == 3){
+        console.log("chorshanba")
+    }else if(hafta == 4){
+        console.log("payshanba")
+    }else if(hafta == 5){
+        console.log("juma")
+    }else if(hafta == 6){
+        console.log("shanba")
+    }else if(hafta == 7){
+        console.log("bazor")
+    }else{
+        console.log("bunaqa kun yuq")
+    }
 }
-grade(90)
-grade(80)
-grade(70)
-grade(60)
-grade(40)
-grade(39)
+haftaKunlari(1)
+haftaKunlari(2)
+haftaKunlari(3)
+haftaKunlari(4)
+haftaKunlari(5)
+haftaKunlari(6)
+haftaKunlari(7)
+haftaKunlari(8)
